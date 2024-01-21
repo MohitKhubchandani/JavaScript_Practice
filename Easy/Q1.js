f23d5b5a3d12538ece4be4a752968922b77c7853
Mohit04 <mohitkhubchandani88@gmail.com>
2024-01-21 22:09:55 +0530

    updated

--- Easy/Q1.js
+++ Easy/Q1.js
@@ -2,10 +2,18 @@
 
 // Create a function that takes two numbers as arguments and returns their sum.
 
+// Examples
+
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
 function sum(Num1,Num2){
     const add = Num1 + Num2;
     return add;
 }
 
-const execute = sum(20,30)
-console.log(execute);
+const addition = sum(20,30)
+console.log(addition); // 50
