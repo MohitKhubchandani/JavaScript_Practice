3ddf3a4e74f76a71d7b2151c3a747d86bd5bb812
Mohit04 <mohitkhubchandani88@gmail.com>
2024-01-23 20:07:48 +0530

    Solved Q3

---
+++ Easy/Q3.js
@@ -0,0 +1,22 @@
+// Return the Next Number from the Integer Passed
+
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+function addition(parameter){
+
+    const output = parameter + 1;
+    
+    return output;
+
+}
+
+const result = addition(5);
+console.log(result);// output :- 6
