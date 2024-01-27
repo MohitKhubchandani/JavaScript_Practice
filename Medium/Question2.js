0e42fc4343cc359a6b68ecfbe8f54f2b05c4d401
Mohit04 <mohitkhubchandani88@gmail.com>
2024-01-27 11:35:58 +0530

    Solved Question2

---
+++ Medium/Question2.js
@@ -0,0 +1,25 @@
+// Find the Smallest and Biggest Numbers
+
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+
+// minMax(1, 2, 3, 4, 5) ➞ [1, 5]
+
+// minMax(2334454, 5) ➞ [5, 2334454]
+
+// minMax(1) ➞ [1, 1]
+
+
+
+function minMax(...value){
+
+  const sort = [...value].sort()
+  const min = Math.min(...sort)
+  const max = Math.max(...sort)
+  
+  return [min ,max];
+}
+
+const Output = minMax(2334454, 5)
+console.log(Output); // Output :- [2334454, 5] 
