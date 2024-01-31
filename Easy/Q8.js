8e1ec010238b6493c511b14ac35842fe889569d5
Mohit04 <mohitkhubchandani88@gmail.com>
2024-01-31 14:31:14 +0530

    Solved Q8

---
+++ Easy/Q8.js
@@ -0,0 +1,21 @@
+// Find the Amount of Potatoes
+
+// Create a function to return the amount of potatoes there are in a string.
+
+// Examples
+
+// potatoes("potato") ➞ 1
+
+// potatoes("potatopotato") ➞ 2
+
+// potatoes("potatoapple") ➞ 1
+
+function potatoes(parameter){
+    const regex = /potato/g;
+    const match = parameter.match(regex);
+    const len = match.length;
+    return len;
+}
+
+console.log(potatoes("potato")); //Output :- 1 
+console.log(potatoes("potatopotato")); //Output :- 2
