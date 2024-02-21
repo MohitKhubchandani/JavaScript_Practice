3195110f602477defa2f8c4eaed84a73ca71bdb2
Mohit04 <mohitkhubchandani88@gmail.com>
2024-02-22 02:24:03 +0530

    Solved leetcode Question 2

---
+++ Medium/Question9.js
@@ -0,0 +1,44 @@
+// LeetCode
+// 344. Reverse String
+
+// Hint
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+
+var reverseString = function(s) {
+    let n = s.length;
+    let i = 0;
+    let  j = n - 1;
+    while(i <= j){
+        // swapping the values
+        let temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+        // move to the next positions
+        i++
+        j--
+    }return s;
+};
+
+const Output = reverseString(["h","e","l","l","o"])
+console.log(Output); // Output :- [ 'o', 'l', 'l', 'e', 'h' ]
