7aced042e9225bfbe76fc0829dd427ee1e991092
Mohit04 <mohitkhubchandani88@gmail.com>
2024-02-29 16:54:15 +0530

    Solved Pattern 1

---
+++ patterns/Pattern1.js
@@ -0,0 +1,17 @@
+// Pattern #1
+
+for(let i = 0; i <= 5 - 1; i++){
+for (let j = 0; j <= 5; j++) {
+    process.stdout.write("*");
+    
+
+} process.stdout.write("\n");}
+
+/**
+ * 
+ *Output :-     ******
+                ******
+                ******
+                ******
+                ******
+ */
