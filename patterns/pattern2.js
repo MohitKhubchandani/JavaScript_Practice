56ffa993f17ce601d5c65d7b726d5094ee825c2b
Mohit04 <mohitkhubchandani88@gmail.com>
2024-03-01 16:39:10 +0530

    Solved Pattern #2

---
+++ patterns/pattern2.js
@@ -0,0 +1,16 @@
+// Pattern #2
+
+for(let i = 0; i <= 5 - 1; i++){
+    for (let j = 0; j <= i; j++) {
+        process.stdout.write("* ");
+        
+    
+    } process.stdout.write("\n");}
+    
+/**
+ * Output :-    *
+                * *
+                * * *
+                * * * *
+                * * * * *
+ */
